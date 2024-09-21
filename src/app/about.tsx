ed09f4dfdc37f184f366e13fc997555947499f54
Rehan Ali <159228403+rehanlaghar@users.noreply.github.com>
2024-09-21 12:43:50 +0500

    Add files via upload

---
+++ src/app/about.tsx
@@ -0,0 +1,10 @@
+function About() {
+    return(
+        <>
+        <h3>This is About Section</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo non quisquam, quam odit molestiae nemo sit aut cumque eaque id repellendus dicta. Dicta, perferendis eaque illo pariatur hic commodi incidunt?
+        </p>
+        </>
+    )
+}
+export default About
---
+++ src/app/header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return(
+<>
+<h2>This is My header</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptas odit quae architecto voluptatibus esse excepturi commodi, quia cumque voluptates quasi, nostrum quibusdam a non! Quod sequi aliquam omnis rerum.</p>
+</>
+    );
+    
+}
+export default Header
